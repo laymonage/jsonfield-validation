15874e12fb2961bb01463466360bf7ce75592b86
sage <laymonage@gmail.com>
2020-10-19 20:59:00 +0700

    validation/models: Create Product and its derivative models

--- validation/app/models.py
+++ validation/app/models.py
@@ -1,3 +1,23 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    stock_qty = models.PositiveIntegerField()
+
+    class Meta:
+        abstract = True
+
+
+class Smartphone(Product):
+    chipset = models.CharField(max_length=255)
+    nfc = models.BooleanField()
+
+
+class Notebook(Product):
+    cpu = models.CharField(max_length=255)
+    gpu = models.CharField(max_length=255)
